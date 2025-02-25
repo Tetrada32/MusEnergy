5b80632c4783f8fc000cd1ea560787d3828d302d
Danil Gahov <danil@gahov.com>
2025-02-25 11:36:28 +0100

    Favorites screen: add "heart" button ui

--- app/src/main/java/com/gahov/musenergy/feature/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/gahov/musenergy/feature/favorites/FavoritesFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,14 +13,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.text.TextStyle
@@ -28,6 +41,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
@@ -80,7 +94,15 @@ class FavoritesFragment : Fragment() {
                 .fillMaxWidth()
         ) {
             Column(Modifier.padding(bottom = 9.dp)) {
-                FavoritesImage((data.articleData.image as IconProvider.Url).url)
+                Box(modifier = Modifier.fillMaxWidth()) {
+                    FavoritesImage((data.articleData.image as IconProvider.Url).url)
+                    RemoveFavoriteArticleButton(
+                        onClick = { },
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(8.dp)
+                    )
+                }
                 ArticleSource(data.articleData.sourceName.getString(requireContext()))
                 ArticleTitle(data.articleData.title.getString(requireContext()))
             }
@@ -95,7 +117,7 @@ class FavoritesFragment : Fragment() {
                 .fillMaxWidth(),
             model = imageUrl,
             contentScale = ContentScale.Crop,
-            contentDescription = "Image"
+            contentDescription = "Favorite Article Image"
         )
     }
 
@@ -130,5 +152,26 @@ class FavoritesFragment : Fragment() {
                 fontWeight = FontWeight.Bold
             )
         )
+    }
+
+    @Composable
+    fun RemoveFavoriteArticleButton(onClick: () -> Unit, modifier: Modifier) {
+        var isClicked by remember { mutableStateOf(false) }
+
+        SmallFloatingActionButton(
+            modifier = modifier,
+            onClick = {
+                isClicked = !isClicked
+                onClick()
+            },
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            contentColor = if (isClicked) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSecondaryContainer
+        ) {
+            if (isClicked) {
+                Icon(Icons.Filled.Favorite, "Button: add article to the favorites")
+            } else {
+                Icon(Icons.Filled.FavoriteBorder, "Button: remove article from favorites")
+            }
+        }
     }
 }
